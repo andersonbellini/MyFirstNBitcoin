5a73422aa55f321194dbded358b1eea6cdd3303f
anderson.bellini <anderson.bellini@anac.gov.br>
2018-10-29 08:52:26 -0300

    Initial Update

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NBitcoin;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Console.WriteLine("My First time using NBitcoin !! ");       
+            Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MyFirstNBitcoin
+
+Example 101 how to use NBitcoin API to security and data transfer
+
+More information in: 
+https://programmingblockchain.gitbook.io/programmingblockchain/
